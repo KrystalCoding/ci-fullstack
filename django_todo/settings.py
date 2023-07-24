d0ad8fca744e8af14c06f1daa4d426b9ff82dd62
KrystalCoding <>
2023-07-24 19:59:11 +0000

    Connecting urls, views, & todos

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,9 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-r7k=twq0ke8=v)ew*-q-+1!(+)@91bwx@-0m(k5rw09&yhh7l*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-krystalcoding-ci-fullsta-7hfqh6dfzi.us2.codeanyapp.com']
 
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
 ]
 
 MIDDLEWARE = [
