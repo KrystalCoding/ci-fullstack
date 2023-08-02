269fee0adb2dbb904ad32895d14ed792f096f472
KrystalCoding <>
2023-08-02 12:06:27 +0000

    Adding heroku to allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,13 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-r7k=twq0ke8=v)ew*-q-+1!(+)@91bwx@-0m(k5rw09&yhh7l*')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-r7k=twq0ke8=v)ew*-q-+1!(+)@91bwx@-0m(k5rw09&yhh7l*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-krystalcoding-ci-fullsta-7hfqh6dfzi.us2.codeanyapp.com',
-                 os.environ.get('HEROKU_HOSTNAME')]
+                 os.environ.get('HEROKU_HOSTNAME'),
+                 'django-portfolio-project-4-ae538e68c8db.herokuapp.com']
 
 
 # Application definition
