91a861326446e9b944986c9776584b6825bc46d8
KrystalCoding <>
2023-08-02 12:32:32 +0000

    Setting up Development environment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,8 +32,11 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = ['8000-krystalcoding-ci-fullsta-7hfqh6dfzi.us2.codeanyapp.com',
-                 os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = ['8000-krystalcoding-ci-fullsta-7hfqh6dfzi.us2.codeanyapp.com',
+                     os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
