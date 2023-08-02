d1ba940ed28d38f1bdc467e3a19a6fb35cc13d51
KrystalCoding <>
2023-08-02 12:04:45 +0000

    Setting up environment variables and auto deployments

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r7k=twq0ke8=v)ew*-q-+1!(+)@91bwx@-0m(k5rw09&yhh7l*'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-r7k=twq0ke8=v)ew*-q-+1!(+)@91bwx@-0m(k5rw09&yhh7l*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-krystalcoding-ci-fullsta-7hfqh6dfzi.us2.codeanyapp.com',
-                 'django-portfolio-project-4-ae538e68c8db.herokuapp.com']
+                 os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
